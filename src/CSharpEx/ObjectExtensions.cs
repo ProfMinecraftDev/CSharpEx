aca4f7516de42ab568dc74ed9606df3b4be0b0cc
ProfMinecraftDev <ProfMinecraftDev@gmail.com>
2026-02-13 21:05:03 -0400

    update de package

--- src/CSharpEx/ObjectExtensions.cs
+++ src/CSharpEx/ObjectExtensions.cs
@@ -1,19 +1,44 @@
 ﻿#nullable enable
+using System;
+
 namespace CSharpEx
 {
 	public static class ObjectExtensions
 	{
 		/// <summary>
-		/// Casteo fluido. Ejemplo: myObj.To<string>();
+		/// Intenta convertir un objeto a cualquier tipo (T), manejando conversiones numéricas y de punteros.
 		/// </summary>
 		public static T To<T>(this object? obj)
 		{
-			return (T)obj!;
+			if (obj == null)
+				return default!;
+
+			Type targetType = typeof(T);
+
+			if (obj.GetType() == targetType)
+			{
+				return (T)obj;
+			}
+
+			if (targetType == typeof(UIntPtr))
+			{
+				return (T)(object)new UIntPtr(Convert.ToUInt64(obj));
+			}
+			if (targetType == typeof(IntPtr))
+			{
+				return (T)(object)new IntPtr(Convert.ToInt64(obj));
+			}
+
+			try
+			{
+				return (T)Convert.ChangeType(obj, targetType);
+			}
+			catch
+			{
+				return (T)obj;
+			}
 		}
 
-		/// <summary>
-		/// Intenta convertir el objeto. Si falla, devuelve el valor por defecto.
-		/// </summary>
 		public static T? As<T>(this object? obj) where T : class
 		{
 			return obj as T;
